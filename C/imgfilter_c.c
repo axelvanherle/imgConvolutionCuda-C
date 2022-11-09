7d378eff50529009e759498a231ac0301f7f2677
Axel Vanherle <axelvanherle@outlook.com>
2022-11-09 09:13:38 +0100

    Update imgfilter_c.c

--- C/imgfilter_c.c
+++ C/imgfilter_c.c
@@ -3,7 +3,9 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image.h"
-#include "stb_image_write.h"    
+#include "stb_image_write.h"  
+
+#define INPUT_IMAGE "img2.png"
 
 struct Pixel
 {
