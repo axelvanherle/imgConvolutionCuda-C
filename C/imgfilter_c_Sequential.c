63b8aba7fb464f0f03af15c057dd65957a82662a
Sem Kirkels <101119408+SemKirkels@users.noreply.github.com>
2022-11-24 14:34:38 +0100

    Start code convolution

--- C/imgfilter_c_Sequential.c
+++ C/imgfilter_c_Sequential.c
@@ -19,7 +19,7 @@
 #define INPUT_IMAGE_8  "Images/img8.png"
 #define INPUT_IMAGE_9  "Images/img9.png"
 
-// Hard coded the output files
+// Hard coded the output files grayscale
 #define OUTPUT_IMAGE_0  "Output_Images/img0.png"
 #define OUTPUT_IMAGE_1  "Output_Images/img1.png"
 #define OUTPUT_IMAGE_2  "Output_Images/img2.png"
@@ -31,8 +31,23 @@
 #define OUTPUT_IMAGE_8  "Output_Images/img8.png"
 #define OUTPUT_IMAGE_9  "Output_Images/img9.png"
 
-void processImage(char *inputFile, char *outputFile, int imgCounter);           // Opens the image, starts the convertion function and writes the output image
+// Hard coded the output files
+#define OUTPUT_IMAGE_0_CONVOLVE  "Output_Images/img0_convolution.png"
+#define OUTPUT_IMAGE_1_CONVOLVE  "Output_Images/img1_convolution.png"
+#define OUTPUT_IMAGE_2_CONVOLVE  "Output_Images/img2_convolution.png"
+#define OUTPUT_IMAGE_3_CONVOLVE  "Output_Images/img3_convolution.png"
+#define OUTPUT_IMAGE_4_CONVOLVE  "Output_Images/img4_convolution.png"
+#define OUTPUT_IMAGE_5_CONVOLVE  "Output_Images/img5_convolution.png"
+#define OUTPUT_IMAGE_6_CONVOLVE  "Output_Images/img6_convolution.png"
+#define OUTPUT_IMAGE_7_CONVOLVE  "Output_Images/img7_convolution.png"
+#define OUTPUT_IMAGE_8_CONVOLVE  "Output_Images/img8_convolution.png"
+#define OUTPUT_IMAGE_9_CONVOLVE  "Output_Images/img9_convolution.png"
+
+void processImageGray(char *inputFile, char *outputFile, int imgCounter);       // Opens the image, starts the convertion function and writes the output image
 void ConvertImageToGrayCpu(unsigned char *imageRGBA, int width, int height);    // Converts the image to grayscale
+
+void processImageConvolve(char *inputFile, char *outputFile, int imgCounter);     // Opens the image, starts the convolution and writes the output file
+void convolveImage(unsigned char *imageRGBA, int width, int height);            // Convolutes image
 
 typedef struct Pixel
 {
@@ -46,34 +61,44 @@ int main()
 
     timer_start = clock(); // start the timer
 
-    processImage(INPUT_IMAGE_0, OUTPUT_IMAGE_0, imgCounter);
+    processImageGray(INPUT_IMAGE_0, OUTPUT_IMAGE_0, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_0, OUTPUT_IMAGE_0_CONVOLVE, imgCounter);
     imgCounter++;
 
-    processImage(INPUT_IMAGE_1, OUTPUT_IMAGE_1, imgCounter);
+    processImageGray(INPUT_IMAGE_1, OUTPUT_IMAGE_1, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_1, OUTPUT_IMAGE_1_CONVOLVE, imgCounter);
     imgCounter++;
 
-    processImage(INPUT_IMAGE_2, OUTPUT_IMAGE_2, imgCounter);
+    processImageGray(INPUT_IMAGE_2, OUTPUT_IMAGE_2, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_2, OUTPUT_IMAGE_2_CONVOLVE, imgCounter);
     imgCounter++;
 
-    processImage(INPUT_IMAGE_3, OUTPUT_IMAGE_3, imgCounter);
+    processImageGray(INPUT_IMAGE_3, OUTPUT_IMAGE_3, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_3, OUTPUT_IMAGE_3_CONVOLVE, imgCounter);
     imgCounter++;
 
-    processImage(INPUT_IMAGE_4, OUTPUT_IMAGE_4, imgCounter);
+    processImageGray(INPUT_IMAGE_4, OUTPUT_IMAGE_4, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_4, OUTPUT_IMAGE_4_CONVOLVE, imgCounter);
     imgCounter++;
 
-    processImage(INPUT_IMAGE_5, OUTPUT_IMAGE_5, imgCounter);
+    processImageGray(INPUT_IMAGE_5, OUTPUT_IMAGE_5, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_5, OUTPUT_IMAGE_5_CONVOLVE, imgCounter);
     imgCounter++;
 
-    processImage(INPUT_IMAGE_6, OUTPUT_IMAGE_6, imgCounter);
+    processImageGray(INPUT_IMAGE_6, OUTPUT_IMAGE_6, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_6, OUTPUT_IMAGE_6_CONVOLVE, imgCounter);
     imgCounter++;
 
-    processImage(INPUT_IMAGE_7, OUTPUT_IMAGE_7, imgCounter);
+    processImageGray(INPUT_IMAGE_7, OUTPUT_IMAGE_7, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_7, OUTPUT_IMAGE_7_CONVOLVE, imgCounter);
     imgCounter++;
 
-    processImage(INPUT_IMAGE_8, OUTPUT_IMAGE_8, imgCounter);
+    processImageGray(INPUT_IMAGE_8, OUTPUT_IMAGE_8, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_8, OUTPUT_IMAGE_8_CONVOLVE, imgCounter);
     imgCounter++;
 
-    processImage(INPUT_IMAGE_9, OUTPUT_IMAGE_9, imgCounter);
+    processImageGray(INPUT_IMAGE_9, OUTPUT_IMAGE_9, imgCounter);
+    processImageConvolve(OUTPUT_IMAGE_9, OUTPUT_IMAGE_9_CONVOLVE, imgCounter);
 
     timer_end = clock(); // end the timer
     double time_spent = (double)(timer_end - timer_start) / CLOCKS_PER_SEC;
@@ -82,7 +107,7 @@ int main()
     return 0;
 }
 
-void processImage(char *inputFile, char *outputFile, int imgCounter)
+void processImageGray(char *inputFile, char *outputFile, int imgCounter)
 {
     // Open image
     int width, height, componentCount;
@@ -123,12 +148,58 @@ void ConvertImageToGrayCpu(unsigned char *imageRGBA, int width, int height)
     {
         for (int x = 0; x < width; x++)
         {
-            Pixel *ptrPixel = (Pixel *)&imageRGBA[y * width * 4 + 4 * x];.
+            Pixel *ptrPixel = (Pixel *)&imageRGBA[y * width * 4 + 4 * x];
             unsigned char pixelValue = (unsigned char)(ptrPixel->r * 0.2126f + ptrPixel->g * 0.7152f + ptrPixel->b * 0.0722f);
             ptrPixel->r = pixelValue;
             ptrPixel->g = pixelValue;
             ptrPixel->b = pixelValue;
             ptrPixel->a = 255;
+        }
+    }
+}
+
+void processImageConvolve(char *inputFile, char *outputFile, int imgCounter)
+{
+    // Open image
+    int width, height, componentCount;
+
+    printf("Loading png file... [%d / 10]\r\n", imgCounter);
+    unsigned char *imageData = stbi_load(inputFile, &width, &height, &componentCount, 4);
+    if (!imageData)
+    {
+        printf("Failed to open Image\r\n");
+        exit(-1);
+    }
+    printf(" DONE \r\n");
+
+    // Validate image sizes
+    if (width % 32 || height % 32)
+    {
+        // NOTE: Leaked memory of "imageData"
+        printf("Width and/or Height is not dividable by 32! [%d / 10]\r\n", imgCounter);
+        exit(-1);
+    }
+
+    // Process image on cpu
+    printf("Processing image...: [%d / 10]\r\n", imgCounter);
+    convolveImage(imageData, width, height);
+    printf(" DONE \r\n");
+
+    // Write image back to disk
+    printf("Writing to disk... [%d / 10]\r\n", imgCounter);
+    stbi_write_png(outputFile, width, height, 4, imageData, 4 * width);
+    printf("DONE [%d / 10]\r\n", imgCounter);
+
+    stbi_image_free(imageData);
+}
+
+void convolveImage(unsigned char *imageRGBA, int width, int height)
+{
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+
         }
     }
 }
